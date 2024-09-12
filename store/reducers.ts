7389b223ed40aa0ed2468a7ab292b4e23a387e85
kxxhan <ofliberty2@gmail.com>
2024-09-12 16:54:43 +0900

    fix: redux error handling

--- store/reducers.ts
+++ store/reducers.ts
@@ -1,4 +1,3 @@
-// reducers/index.js
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -12,25 +11,25 @@ import taskSlice from "./taskSlice";
 import canvasReducer from "./canvasSlice";
 import propertyPanelReducer from "./propertyPanelSlices";
 
-export const rootReducer = combineReducers({
-    network: networkReducer,
-    setting: settingReducer,
-    status: statusReducer,
-    connection: connectionSlice,
-    canvas: canvasReducer,
-    propertyPanel: propertyPanelReducer,
-    user: userReducer,
-    task: taskSlice
+const rootReducer = combineReducers({
+  network: networkReducer,
+  setting: settingReducer,
+  status: statusReducer,
+  connection: connectionSlice,
+  canvas: canvasReducer,
+  propertyPanel: propertyPanelReducer,
+  user: userReducer,
+  task: taskSlice,
 });
 
 const persistConfig = {
-    key: "root",
-    // localStorage에 저장합니다.
-    storage,
-    // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
-    whitelist: ["user", "network"]
-    // blacklist -> 그것만 제외합니다
-  };
+  key: "root",
+  // localStorage에 저장합니다.
+  storage,
+  // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
+  whitelist: ["user", "network"],
+  // blacklist -> 그것만 제외합니다
+};
 
-  
-export default persistReducer(persistConfig, rootReducer);
+export default persistReducer(persistConfig, rootReducer) as typeof rootReducer;
+
--- store/store.tsx
+++ store/store.tsx
@@ -1,43 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useMemo } from "react";
-import { createWrapper } from "next-redux-wrapper";
-import networkReducer from "./networkSlice";
-import userReducer from "./userSlice";
-import settingReducer from "./settingSlice";
-import statusReducer from "./statusSlice";
-import connectionSlice from "./connectionSlice";
-import taskSlice from "./taskSlice";
-import canvasReducer from "./canvasSlice";
-import propertyPanelReducer from "./propertyPanelSlices";
-import { rootReducer } from "./reducers";
-import { PersistGate } from "redux-persist/integration/react";
-import thunk from 'redux-thunk';
-
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // localStorage를 사용할 경우
-const persistConfig = {
-  key: 'root',
-  // version: 1,
-  storage: storage
-};
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+import rootReducer from "./reducers";
+import { persistStore } from "redux-persist";
 
 export const store = configureStore({
-  reducer:persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-        serializableCheck: false
-        // {
-        //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-        // },
+      serializableCheck: false,
     }),
 });
 export const persistor = persistStore(store);
